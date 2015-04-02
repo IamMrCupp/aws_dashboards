6742bd35d086d3f95c656de8b44f2dc8b0af54a3
Aaron Cupp <mrcupp@mrcupp.com>
2015-04-02 11:07:21 -0700

    changes made and sync'd back to local repos

--- jobs/foreman.rb
+++ jobs/foreman.rb
@@ -5,34 +5,40 @@
 require "net/https"
 require "uri"
 
-hostname="foreman.ue1.prod.study.com"
+uri = URI('https://foreman.ue1.prod.study.com/api/v2/dashboard')
 
-SCHEDULER.every '4s' do
-  http = Net::HTTP.new(hostname,443)
-  http.use_ssl = true
-  http.ssl_version = :SSLv3
-  request = Net::HTTP::Get.new("/api/v2/dashboard")
-  request.basic_auth("wally", 'igetinfoforthings')
-  response = http.request(request)
 
-  hosts = JSON.parse(response.body)["total_hosts"]
-  warnings = JSON.parse(response.body)["out_of_sync_hosts"]
-  errors = JSON.parse(response.body)["bad_hosts"]
- 
-  if errors > 0 then
-    value = errors
-    color = 'red' + "-blink"
-  elsif warnings > 0
-    value = warnings
-    color = 'yellow'
-  else
-    value = hosts
-    color = 'green'
-  end
+SCHEDULER.every '5s' do
+  Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https',
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
-  send_event('foreman', {
-    value: value,
-    color: color
-  })
-end
+    request = Net::HTTP::Get.new uri.request_uri
+    request.basic_auth 'wally', 'igetinfoforthings'
+
+    response = http.request request # Net::HTTPRepose object
+    
+    # debug output if good...sometimes
+    #puts response
+    #puts response.body
+
+    hosts = JSON.parse(response.body)["total_hosts"]
+    warnings = JSON.parse(response.body)["out_of_sync_hosts"]
+    errors = JSON.parse(response.body)["bad_hosts"]
+
+    if errors > 0 then
+      value = errors
+      color = 'red' + "-blink"
+    elsif warnings > 0
+      value = warnings
+      color = 'yellow'
+    else
+      value = hosts
+      color = 'green'
+    end   #end if block
+
+    send_event('foreman', {value: value, color: color})
+  end   #end Net::HTTP.start block
+end  #end scheduler code block
+
 
